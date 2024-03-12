d27de708e1d4efc303cb9ed4119499ff5f76400e
Sueyoshi Ryouga <r.sueyosi.humanghm@gmail.com>
2024-03-12 14:04:05 +0900

    [add]  UniRx

---
+++ MVP_RPG_Unity/Assets/Script/Model/PlayerModel.cs
@@ -0,0 +1,19 @@
+
+namespace MVRP.Models
+{
+    public class PlayerModel
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
+
